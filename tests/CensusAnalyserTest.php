43e049d9ef44ed5f18f8055e123e748f20ef2f9b
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-20 20:23:24 +0530

    Added: Test case to get the states in alphabetical order

--- tests/CensusAnalyserTest.php
+++ tests/CensusAnalyserTest.php
@@ -1,29 +1,36 @@
 <?php
 
-    class CensusAnalyserTest extends \PHPUnit\Framework\TestCase{
-
-        public function testGivenACsvFile_WhenCountsTheRows_ShouldReturnNumberofRows(){
-            $numberOfRecords = new \App\Service\CensusAnalyser;
-            $this->assertEquals(count($numberOfRecords->readCSV()),29);
-        }
-
-        public function testGivenACsvFile_WhenCheckedForMostPopulatedState_ShouldReturnMostPopulatedState(){
-            $mostPopulatedState = new \App\Service\CensusAnalyser;
-            $this->assertEquals($mostPopulatedState->sortDataByPopulation(),'Uttar Pradesh');
-        }
-
-        public function testGivenACsvFile_WhenCheckedForLargestStateInArea_ShouldReturnTheLargestState(){
-            $largestStateInArea = new \App\Service\CensusAnalyser;
-            $this->assertEquals($largestStateInArea->sortDataByAreaInSquareKm(),'Rajasthan');
-        }
-
-        public function testGivenACsvFile_WhenCheckedForDensityInPopulation_ShouldReturnMostDenslyPopulated(){
-            $mostDenslyPopulated = new \App\Service\CensusAnalyser;
-            $this->assertEquals($mostDenslyPopulated->sortDataByDenslyPopulated(),'Maharashtra');
-        }
-
-        
-        
+class CensusAnalyserTest extends \PHPUnit\Framework\TestCase
+{
+    
+    public function testGivenACsvFile_WhenCountsTheRows_ShouldReturnNumberofRows()
+    {
+        $numberOfRecords = new \App\Service\CensusAnalyser;
+        $this->assertEquals(count($numberOfRecords->readCSV()), 29);
     }
 
-?>
+    public function testGivenACsvFile_WhenSortedByAlphabeticaOrder_ShouldReturnStateWithAlphabeticallyCorrect()
+    {
+        $alphabeticalOrder = new \App\Service\CensusAnalyser;
+        $this->assertEquals($alphabeticalOrder->sortStateNamesByAlphabeticalOrder(), 'Andhra Pradesh'); 
+
+    }
+
+    public function testGivenACsvFile_WhenCheckedForMostPopulatedState_ShouldReturnMostPopulatedState()
+    {
+        $mostPopulatedState = new \App\Service\CensusAnalyser;
+        $this->assertEquals($mostPopulatedState->sortDataByPopulation(), 'Uttar Pradesh');
+    }
+
+    public function testGivenACsvFile_WhenCheckedForLargestStateInArea_ShouldReturnTheLargestState()
+    {
+        $largestStateInArea = new \App\Service\CensusAnalyser;
+        $this->assertEquals($largestStateInArea->sortDataByAreaInSquareKm(), 'Rajasthan');
+    }
+
+    public function testGivenACsvFile_WhenCheckedForDensityInPopulation_ShouldReturnMostDenslyPopulated()
+    {
+        $mostDenslyPopulated = new \App\Service\CensusAnalyser;
+        $this->assertEquals($mostDenslyPopulated->sortDataByDenslyPopulated(), 'Bihar');
+    }
+}
