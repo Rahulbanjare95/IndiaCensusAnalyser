da07e5b7a941f7c536d6a5a65e7670e61ee10e4c
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-21 11:12:50 +0530

    Added: Test cases to get the least and smallest states in all sorting criterias like population , area and density of populataion

--- tests/CensusAnalyserTest.php
+++ tests/CensusAnalyserTest.php
@@ -9,28 +9,59 @@ class CensusAnalyserTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals(count($numberOfRecords->readCSV()), 29);
     }
 
-    public function testGivenACsvFile_WhenSortedByAlphabeticaOrder_ShouldReturnStateWithAlphabeticallyCorrect()
+    public function testGivenACsvFile_WhenSortedByAlphabeticaOrder_ShouldReturnStateWithAlphabeticallyFirst()
     {
         $alphabeticalOrder = new \App\Service\CensusAnalyser;
-        $this->assertEquals($alphabeticalOrder->sortStateNamesByAlphabeticalOrder(), 'Andhra Pradesh'); 
+        $array=$alphabeticalOrder->sortStateNamesByAlphabeticalOrder();
+        $this->assertEquals($array[0][0][0], 'Andhra Pradesh'); 
+    }
 
+    public function testGivenACsvFile_WhenSortedByAlphabeticaOrder_ShouldReturnStateWithAlphabeticallyLast()
+    {
+        $alphabeticalOrder = new \App\Service\CensusAnalyser;
+        $array=$alphabeticalOrder->sortStateNamesByAlphabeticalOrder();
+        $this->assertEquals($array[0][28][0], 'West Bengal'); 
     }
 
     public function testGivenACsvFile_WhenCheckedForMostPopulatedState_ShouldReturnMostPopulatedState()
     {
         $mostPopulatedState = new \App\Service\CensusAnalyser;
-        $this->assertEquals($mostPopulatedState->sortDataByPopulation(), 'Uttar Pradesh');
+        $populatedState = $mostPopulatedState->sortDataByPopulation();
+        $this->assertEquals($populatedState[0][0][0], 'Uttar Pradesh');
+    }
+
+    public function testGivenACsvFile_WhenCheckedForMostPopulatedState_ShouldReturnLeastPopulatedState()
+    {
+        $mostPopulatedState = new \App\Service\CensusAnalyser;
+        $populatedState = $mostPopulatedState->sortDataByPopulation();
+        $this->assertEquals($populatedState[0][28][0], 'Sikkim');
     }
 
     public function testGivenACsvFile_WhenCheckedForLargestStateInArea_ShouldReturnTheLargestState()
     {
         $largestStateInArea = new \App\Service\CensusAnalyser;
-        $this->assertEquals($largestStateInArea->sortDataByAreaInSquareKm(), 'Rajasthan');
+        $largestAreaState = $largestStateInArea->sortDataByAreaInSquareKm();
+        $this->assertEquals($largestAreaState[0][0][0], 'Rajasthan');
+    }
+
+    public function testGivenACsvFile_WhenCheckedForSmallersStateInArea_ShouldReturnTheSmallestState()
+    {
+        $largestStateInArea = new \App\Service\CensusAnalyser;
+        $largestAreaState = $largestStateInArea->sortDataByAreaInSquareKm();
+        $this->assertEquals($largestAreaState[0][28][0], 'Goa');
     }
 
     public function testGivenACsvFile_WhenCheckedForDensityInPopulation_ShouldReturnMostDenslyPopulated()
     {
         $mostDenslyPopulated = new \App\Service\CensusAnalyser;
-        $this->assertEquals($mostDenslyPopulated->sortDataByDenslyPopulated(), 'Bihar');
+        $desnlyPopulatedStates = $mostDenslyPopulated->sortDataByDenslyPopulated();
+        $this->assertEquals($desnlyPopulatedStates[0][0][0], 'Bihar');
+    }
+
+    public function testGivenACsvFile_WhenCheckedForDensityInPopulation_ShouldReturnLeastDenslyPopulated()
+    {
+        $mostDenslyPopulated = new \App\Service\CensusAnalyser;
+        $desnlyPopulatedStates = $mostDenslyPopulated->sortDataByDenslyPopulated();
+        $this->assertEquals($desnlyPopulatedStates[0][28][0], 'Arunachal Pradesh');
     }
 }
